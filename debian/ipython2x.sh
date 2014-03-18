ae67f7c37906631e063e4162c338c6140b7d4239
Yaroslav Halchenko <debian@onerussian.com>
2014-03-17 23:21:06 -0400

    sweep of changes for ipython2x

---
+++ debian/ipython2x.sh
@@ -0,0 +1,12 @@
+#! /bin/sh
+
+# determine desired Python version from filename
+VERSION="${0##*ipython2x}"
+
+if [ ! -f /usr/bin/python$VERSION ]
+then
+	echo "Please install the python$VERSION package." >&2
+	exit 1
+else
+	exec python$VERSION -c "import sys; sys.argv[0] = '/usr/bin/ipython2x$VERSION'; from IPython.frontend.terminal.ipapp import launch_new_instance; launch_new_instance()" $@
+fi
